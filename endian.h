9b720f6cab2fe99d7fbde913f9620fc64b76d8ba
rdpoor <rdpoor@gmail.com>
2018-03-15 10:32:52 -0700

    Remove COERCE_PTR, add MEMCPY_LE and MEMCPY_BE

--- endian.h
+++ endian.h
@@ -31,38 +31,56 @@ SOFTWARE.
 #include <stdint.h>
 
 /**
- * COERCE_PTR(type, p) 
+ * For pointer-to-pointer transfers, endian provides the MEMCPY_LE and MEMCPY_BE
+ * macros:
  *
- * @param type a C language type
- * @param p    a pointer to some storage (register or memory)
+ * double d;
+ * char buf[80]
  *
- * @note
- * The COERCE_PTR macro allows you to cast a pointer of an arbitrary C type to
- * any other C pointer type. For example:
+ * // copy double d into buf[4] as a little endian value
+ * MEMCPY_LE(&buf[4], &d, sizeof(double))
  *
- *     float f;
- *     uint32_t i = *COERCE_PTR(uint32_t, &f)
- *     printf("Bit pattern for %f is 0x%x\n", f, i);
+ * // copy double d into buf[4] as a big endian value
+ * MEMCPY_BE(&buf[4], &d, sizeof(double))
  *
- * Alternatively:
- *
- *     float f;
- *     uint32_t *i = COERCE_PTR(uint32_t, &f)
- *     printf("Bit pattern for %f is 0x%x\n", f, *i);
+ * but it also provide a family of getters and putters:
+ * 
+ * double get_double_be(void *src);
+ * double get_double_le(void *src);
+ * void put_double_be(void *dst, double v);
+ * void put_double_le(void *dst, double v);
+ * ... etc ...
  */
-#define COERCE_PTR(type, v) ((type *)v)
 
-uint8_t get_uint8_le(void *src); // 'a needless consistancy...' :)
-uint8_t get_uint8_be(void *src);
+// The __BYTE_ORDER__ compiler macro defines the byte ordering on the target
+// CPU.  If it is little endian, little endian operations to use the native
+// memcpy and use revcpy for big endian operations.  And vice versa for big
+// endian CPUs.
 
-uint16_t get_uint16_le(void *src);
-uint16_t get_uint16_be(void *src);
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define MEMCPY_LE memcpy
+#define MEMCPY_BE revcpy
+#else
+#define MEMCPY_LE revcpy
+#define MEMCPY_BE memcpy
+#endif
 
-uint32_t get_uint32_le(void *src);
-uint32_t get_uint32_be(void *src);
+/**
+ * revcpy is like memcpy but with byte reversal
+ */
+void revcpy(void *dst, const void *src, int nbytes);
 
-uint64_t get_uint64_le(void *src);
-uint64_t get_uint64_be(void *src);
+uint8_t get_uint8_t_le(void *src); // 'a needless consistancy...' :)
+uint8_t get_uint8_t_be(void *src);
+
+uint16_t get_uint16_t_le(void *src);
+uint16_t get_uint16_t_be(void *src);
+
+uint32_t get_uint32_t_le(void *src);
+uint32_t get_uint32_t_be(void *src);
+
+uint64_t get_uint64_t_le(void *src);
+uint64_t get_uint64_t_be(void *src);
 
 float get_float_le(void *src);
 float get_float_be(void *src);
@@ -71,17 +89,17 @@ double get_double_le(void *src);
 double get_double_be(void *src);
 
 
-void put_uint8_le(void *dst, uint8_t v);
-void put_uint8_be(void *dst, uint8_t v);
+void put_uint8_t_le(void *dst, uint8_t v);
+void put_uint8_t_be(void *dst, uint8_t v);
 
-void put_uint16_le(void *dst, uint16_t v);
-void put_uint16_be(void *dst, uint16_t v);
+void put_uint16_t_le(void *dst, uint16_t v);
+void put_uint16_t_be(void *dst, uint16_t v);
 
-void put_uint32_le(void *dst, uint32_t v);
-void put_uint32_be(void *dst, uint32_t v);
+void put_uint32_t_le(void *dst, uint32_t v);
+void put_uint32_t_be(void *dst, uint32_t v);
 
-void put_uint64_le(void *dst, uint64_t v);
-void put_uint64_be(void *dst, uint64_t v);
+void put_uint64_t_le(void *dst, uint64_t v);
+void put_uint64_t_be(void *dst, uint64_t v);
 
 void put_float_le(void *dst, float v);
 void put_float_be(void *dst, float v);
